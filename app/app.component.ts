4e6f51affffc9650805b54fba5ec6df36a281153
Martin Roldan Araujo <rataciber@MacBook-Pro-de-Martin.local>
2017-03-10 16:05:30 -0500

    Added required files

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
     template: '<h1>Ratacibernetica Skeleton project</h1>'
-}); 
+})
 
 export class AppComponent {} 
