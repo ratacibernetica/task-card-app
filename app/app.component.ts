1b3343e949ebdf61f2c8028726d57e465deae4bc
Martin Roldan Araujo <rataciber@MacBook-Pro-de-Martin.local>
2017-03-11 23:29:06 -0500

    Clear input field after adding a task.

--- app/app.component.ts
+++ app/app.component.ts
@@ -25,5 +25,6 @@ export class AppComponent {
     addTask() {
         let task = new Task(this.currentTask.content, this.currentTask.completed);
         this.tasks.push(task);
+        this.currentTask.content = null;
     }
 } 
